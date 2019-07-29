2bca45a85c38cf23f31577bc6ee9b30f94e28477
zombodotcom <tommysciano@gmail.com>
2019-07-29 08:02:33 -0600

    fix

---
+++ Java/CodingInterviewPrograms/src/zombo/HelloWorld.java
@@ -0,0 +1,9 @@
+package zombo;
+
+public class HelloWorld {
+  public static void HelloWorld() {
+    // Prints "Hello, World" to the terminal window.
+    System.out.println("Hello, World");
+  }
+
+}
---
+++ Java/CodingInterviewPrograms/src/zombo/Main.java
@@ -0,0 +1,10 @@
+package zombo;
+
+public class Main {
+  public static void main(String[] args) {
+    // Prints "Hello, World" to the terminal window.
+    System.out.println("Hello, World");
+    HelloWorld.HelloWorld();
+  }
+
+}
